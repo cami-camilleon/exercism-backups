702870ea427ee715799f7431f375549f258c3135
Camille Phelps <phelpscg@icloud.com>
2026-03-01 08:30:26 -0800

    solved raindrops

--- python/03-conditionals/raindrops/raindrops.py
+++ python/03-conditionals/raindrops/raindrops.py
@@ -1,2 +1,12 @@
 def convert(number):
-    pass
+    raindrops = ''
+    if number % 3 == 0:
+        raindrops = raindrops + 'Pling'
+    if number % 5 == 0:
+        raindrops = raindrops + 'Plang'
+    if number % 7 == 0:
+        raindrops = raindrops + 'Plong'
+    if len(raindrops) == 0:
+        raindrops = f'{number}'
+    
+    return raindrops
