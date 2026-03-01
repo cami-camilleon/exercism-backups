551d09b60c3e6a2d27c1f6d35ff50a9b62b0e912
Camille Phelps <phelpscg@icloud.com>
2026-03-01 10:24:53 -0800

    solved pig-latin

--- python/03-conditionals/pig-latin/pig_latin.py
+++ python/03-conditionals/pig-latin/pig_latin.py
@@ -1,2 +1,35 @@
+VOWELS = 'aeiou'
+
+def check_for_vowels(text):
+    for char in text:
+        if char in VOWELS:
+            return True
+    return False
+
+def find_first_vowel(text):
+    for index, char in enumerate(text):
+        if char in VOWELS:
+            return index
+    return False
+
 def translate(text):
-    pass
+    res = []
+    for word in text.split(' '):
+        if check_for_vowels(word[0]) or word[0:2] in ['xr', 'yt']:
+            res.append(f'{word}ay')
+        else:
+            if (word.find('qu') < 0 and word.find('y') <= 0) or (check_for_vowels(word[0:word.find('qu')]) and check_for_vowels(word[0:word.find('y')])):
+                # answer is not qu or y
+                # if word in ['queen', 'quick', 'square']:return not check_for_vowels(word[0:word.find('qu')])
+                res.append(f'{word[find_first_vowel(word):]}{word[0:find_first_vowel(word)]}ay')
+            else:
+                if word.find('qu') >= 0 and (word.find('qu') < word.find('y') or word.find('y') == -1):
+                    # do the qu one
+                    # if word in ['queen', 'quick', 'square']:return 2
+                    # find_first_vowel should return index of the u in 'qu'
+                    res.append(f'{word[find_first_vowel(word) + 1:]}{word[0:find_first_vowel(word) + 1]}ay')
+                else:
+                    # do the y one
+                    res.append(f'{word[word.find('y'):]}{word[0:word.find('y')]}ay')
+
+    return ' '.join(res)
