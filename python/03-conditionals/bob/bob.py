b182a1d401f43f8205be87a3e3172b357b225cc4
Camille Phelps <phelpscg@icloud.com>
2026-03-01 08:21:59 -0800

    solved bob

--- python/03-conditionals/bob/bob.py
+++ python/03-conditionals/bob/bob.py
@@ -1,2 +1,16 @@
 def response(hey_bob):
-    pass
+    response = ''
+
+    if hey_bob.isupper() and not hey_bob.islower() and len(hey_bob.strip()) > 0:
+        if hey_bob.rstrip().endswith("?"):
+            response = "Calm down, I know what I'm doing!"
+        else:
+            response = "Whoa, chill out!"
+    elif hey_bob.rstrip().endswith("?"):
+        response = "Sure."
+    elif len(hey_bob.strip()) == 0:
+        response = "Fine. Be that way!"
+    else:
+        response = "Whatever."
+
+    return response
